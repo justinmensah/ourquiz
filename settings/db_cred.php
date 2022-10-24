016fa2187c53dd1fcbf48c6ce81ce25209d1bd8a
geraldarko <gerald.darko@ashesi.edu.gh>
2022-10-24 10:29:39 +0000

    changes

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,9 +1,10 @@
 <?php
 //Database credentials
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'class_contact_mgmt');
+define('DB_SERVER', 'us-cdbr-east-06.cleardb.net');
+define('DB_USERNAME', 'ba31481e1bdb22');
+define('DB_PASSWORD', 'bd1ea1c9');
+define('DB_NAME', 'heroku_b950ade82d3ea80');
 
+//mysql://ba31481e1bdb22:bd1ea1c9@us-cdbr-east-06.cleardb.net/heroku_b950ade82d3ea80?reconnect=true
 
 ?>
